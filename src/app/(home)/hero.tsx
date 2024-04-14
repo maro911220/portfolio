3e51a7cbbed2a749c00b730101fab3870ab5c94b
maro911220 <kingdomboze@gmail.com>
2024-04-14 14:02:33 +0900

    update240414

--- src/app/(home)/hero.tsx
+++ src/app/(home)/hero.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import { sectionRef } from "@/types/useTypes";
-import Image from "next/image";
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, useAnimations, Box, SoftShadows } from "@react-three/drei";
+import { useEffect } from "react";
 
 export default function Hero({ Ref }: { Ref: sectionRef }) {
   return (
@@ -11,26 +15,74 @@ export default function Hero({ Ref }: { Ref: sectionRef }) {
             Ref.current[0] = e;
           }}
         >
-          <h2 className="home-hero-con__title">
-            I am Maro <br />
-            Creative Web Developer.
-          </h2>
+          <h2 className="home-hero-con__title">Hi, I am Maro</h2>
+          <p className="home-hero-con__title"> I&apos;m web publisher</p>
           <p className="home-hero-con__sub">
-            i develop accessible, responsive, interactive, and animated websites
-            <br />
-            with a strong focus on perfomance
+            and I&apos;m aiming to be a front-end developer.
           </p>
         </div>
         <div className="home-hero-imgbox">
-          <Image
-            className="home-hero-img"
-            src="/images/main.png"
-            alt="메인 이미지"
-            width={1024}
-            height={1024}
-          />
+          <div className="home-hero-imgbox-canvas">
+            <Canvas shadows camera={{ position: [0, 0, 10], fov: 20 }}>
+              {/* Lightmap */}
+              <directionalLight
+                intensity={10}
+                color="#eee8df"
+                position={[-5, 2, 3]}
+              />
+              <spotLight intensity={1} castShadow />
+              {/* model */}
+              <Model />
+              {/* shadow */}
+              <Shadows />
+            </Canvas>
+          </div>
         </div>
       </div>
     </section>
+  );
+}
+
+function Model() {
+  const { scene, animations } = useGLTF("/modeling/test.glb");
+  const { ref, actions, names } = useAnimations(animations);
+  useEffect(() => {
+    actions[names[0]]?.reset().play();
+  }, []);
+
+  return (
+    <primitive
+      castShadow
+      receiveShadow
+      object={scene}
+      ref={ref}
+      scale={0.8}
+      position={[0, -1, 0]}
+      rotation={[0.1, -0.4, 0]}
+    />
+  );
+}
+
+function Shadows() {
+  return (
+    <>
+      <Box
+        position={[0.01, -0.38, -0.35]}
+        args={[0.3, 1.5, 1.2]}
+        rotation={[0, 0, 0]}
+        castShadow
+      >
+        <shadowMaterial transparent opacity={0} />
+      </Box>
+      <mesh
+        rotation={[-0.5 * Math.PI, 0.02, 0]}
+        position={[0, -1.2, 0]}
+        receiveShadow
+      >
+        <planeGeometry args={[10, 10]} />
+        <shadowMaterial transparent color="#c18b3b" opacity={0.8} />
+      </mesh>
+      <SoftShadows size={100} samples={100} />
+    </>
   );
 }
