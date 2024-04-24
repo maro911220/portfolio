d6e0ff51d9069e98aa894d082a65a077cf63cf6f
maro911220 <kingdomboze@gmail.com>
2024-04-25 00:11:53 +0900

    back 240424

--- src/app/(home)/contact.tsx
+++ src/app/(home)/contact.tsx
@@ -3,6 +3,7 @@ import { sectionRef } from "@/types/useTypes";
 export default function Contact({ Ref }: { Ref: sectionRef }) {
   return (
     <section
+      className="home-contact"
       ref={(e) => {
         Ref.current[4] = e;
       }}
--- src/app/(home)/work.tsx
+++ src/app/(home)/work.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
@@ -33,13 +33,21 @@ const workList = [
 
 export default function Work({ Ref }: { Ref: sectionRef }) {
   const container = useRef<HTMLElement>(null);
+  const [resizeCheck, setResizeCheck] = useState(0);
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setResizeCheck(window.innerWidth);
+    });
+  }, []);
+
   useGSAP(
     (e: any) => {
-      const item = e.selector(".home-work-wrap")[0];
-      const itemWidth = item.clientWidth;
-      const x = itemWidth - Number(container.current?.clientWidth);
+      const list = e.selector(".home-work-wrap")[0];
+      const listWidth = list.clientWidth;
+      const wrapWidth = e.selector(".home-work-con")[0].clientWidth;
+      const x = listWidth - wrapWidth;
 
-      gsap.to(item, {
+      gsap.to(list, {
         x: -x,
         ease: "none",
         scrollTrigger: {
@@ -47,48 +55,49 @@ export default function Work({ Ref }: { Ref: sectionRef }) {
           scrub: 1,
           pin: true,
           start: "top",
-          end: itemWidth,
+          end: listWidth,
         },
       });
     },
-    { scope: container }
+    { dependencies: [resizeCheck], scope: container, revertOnUpdate: true }
   );
 
   return (
-    // portfolio-container / portfolio-list / portfolio-list-item
     <section className="home-work" ref={container}>
       <h2 className="hidden">Work</h2>
-      <article className="home-work-scroller">
-        <div
-          className="home-work-con"
-          ref={(e) => {
-            Ref.current[3] = e;
-          }}
-        >
-          <div className="home-work-wrap">
-            <div className="home-work-hero">
-              <h3 className="home-work-hero__title fs-fr">Work</h3>
-              <p className="home-work-hero__sub">
-                A selection of our crafted work, built from scratch by our
-                experienced team.
-              </p>
+      <article className="home-work-scroller-dummy">
+        <div className="home-work-scroller">
+          <div
+            className="home-work-con"
+            ref={(e) => {
+              Ref.current[3] = e;
+            }}
+          >
+            <div className="home-work-wrap">
+              <div className="home-work-hero">
+                <h3 className="home-work-hero__title fs-fr">Work</h3>
+                <p className="home-work-hero__sub">
+                  A selection of our crafted work, built from scratch by our
+                  experienced team.
+                </p>
+              </div>
+              <ul className="home-work-list">
+                {workList.map((item, index) => {
+                  return (
+                    <li className="home-work-item" key={index}>
+                      <a className="home-work-item__link" href={item.link}>
+                        <p className="home-work-item__title">{item.name}</p>
+                        <div className="home-wrok-item__box">
+                          {item.type.map((label, index) => {
+                            return <span key={index}>{label}</span>;
+                          })}
+                        </div>
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
-            <ul className="home-work-list">
-              {workList.map((item, index) => {
-                return (
-                  <li className="home-work-item" key={index}>
-                    <a className="home-work-item__link" href={item.link}>
-                      <p className="home-work-item__title">{item.name}</p>
-                      <div className="home-wrok-item__box">
-                        {item.type.map((label, index) => {
-                          return <span key={index}>{label}</span>;
-                        })}
-                      </div>
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
           </div>
         </div>
       </article>
