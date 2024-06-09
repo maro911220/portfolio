181585226fbfac80604e34adde4f1001f220d7c7
maro911220 <kingdomboze@gmail.com>
2024-06-09 15:59:34 +0900

    lottie 애니메이션 종료 후 GSAP 애니메이션 실행 처리

--- src/app/(layout)/loading.tsx
+++ src/app/(layout)/loading.tsx
@@ -10,8 +10,8 @@ const loadingStyle = `fixed flex justify-center items-center top-0 left-0 w-dvw 
 
 // loading 컴포넌트
 export default function Loading() {
-  const loadingRef = useRef(null); // 로딩 컨테이너 참조
-  const lottieRef = useRef(null); // 로티 애니메이션 참조
+  const loadingRef = useRef<HTMLDivElement>(null); // 로딩 컨테이너 참조
+  const lottieRef = useRef<any>(null); // 로티 애니메이션 참조
   const { setFirstLoad } = useStore(defaultStore); // zustand
 
   // 로티 파일 동적 임포트
@@ -22,23 +22,28 @@ export default function Loading() {
   // GSAP 애니메이션 설정
   useGSAP(
     () => {
-      gsap.to(loadingRef.current, {
-        opacity: 0,
-        delay: 3,
-        duration: 1,
-        onStart: () => {
-          setFirstLoad();
-          document.body.classList.add("active");
-        },
-      });
+      const lottieElement = lottieRef.current;
+      if (lottieElement) {
+        // 로티 애니메이션 종료 후 실행
+        lottieElement.addEventListener("complete", () => {
+          gsap.to("#loading-lottie", {
+            y: 100,
+            opacity: 0,
+          });
 
-      gsap.to("#loading-lottie", {
-        delay: 2.5,
-        y: 100,
-        opacity: 0,
-      });
+          gsap.to(loadingRef.current, {
+            opacity: 0,
+            delay: 0.5,
+            duration: 1,
+            onStart: () => {
+              setFirstLoad();
+              document.body.classList.add("active");
+            },
+          });
+        });
+      }
     },
-    { scope: loadingRef }
+    { dependencies: [lottieRef], scope: loadingRef }
   );
 
   return (
