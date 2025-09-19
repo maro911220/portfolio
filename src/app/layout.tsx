853bbb6f3a3b629611b6b2f3116c431679cd250f
maro911220 <kingdomboze@gmail.com>
2025-09-19 10:31:31 +0900

    metadata 개선

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,21 +6,29 @@ import "@/styles/globals.scss";
 // 메타 정보
 const title = "Maro Portfolio";
 const url = "https://maro-portfolio.vercel.app";
-const des = "MARO의 포트폴리오 페이지 입니다.";
+const description = "MARO의 포트폴리오 페이지 입니다.";
 
 // 사이트 메타데이터 설정
 export const metadata: Metadata = {
-  title: title,
-  description: des,
-  icons: { icon: "/favicon.ico" },
   metadataBase: new URL(url),
+  title,
+  description,
+  icons: { icon: "/favicon.ico" },
   openGraph: {
-    title: title,
+    title,
     siteName: title,
-    url: url,
-    description: des,
-    images: "/tumb.jpg",
+    url,
+    description,
+    images: [
+      {
+        url: "/tumb.jpg",
+        width: 800,
+        height: 400,
+        alt: title,
+      },
+    ],
     type: "website",
+    locale: "ko_KR",
   },
 };
 
