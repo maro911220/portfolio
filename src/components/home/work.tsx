1a298b2d01f444595a04f52815e04fd1b42363e9
maro911220 <kingdomboze@gmail.com>
2025-10-14 20:24:50 +0900

    리스트 업데이트

--- src/components/home/work.tsx
+++ src/components/home/work.tsx
@@ -19,6 +19,13 @@ const WORK_LIST = [
     src: "/images/web/web_1.jpg",
   },
   {
+    name: "My sss",
+    sub: "[개인 프로젝트] 홍대 지역의 굿즈샵 정보를 확인할 수 있는 웹 애플리케이션",
+    link: "https://maro-board.vercel.app/",
+    type: ["React", "Next.js", "TailwindCSS", "KAKAO API"],
+    src: "/images/web/web_6.jpg",
+  },
+  {
     name: "My Dashboard",
     sub: "[개인 프로젝트] 실시간 영화/미디어 정보 및 날씨 확인이 가능한 검색형 대시보드",
     link: "https://maro-board.vercel.app/",
